83d1cb69c2bdf8b1ad979d48baeaec42ee7bd561
Liam Parker <weparkervsc@gmail.com>
2025-09-29 16:44:04 -0400

    first commit

---
+++ Driver.java
@@ -0,0 +1,9 @@
+//Programmed by:
+//Assignment: Project One: Energy Account
+//Purpose (of this file): Driver file for class, actually goes in and completes the assigned outputs and calculations
+
+public class Driver {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ EnergyAccount.java
@@ -0,0 +1,7 @@
+//Programmed by: 
+//Assignment: Project One: Energy Account
+//Purpose (of this file): Declare and create the class used in driver.java
+
+public class EnergyAccount {
+
+}
